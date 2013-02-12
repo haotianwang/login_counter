64d4b939ad21b2d28f5eb32d51c1b4147b4aa210
haotian wang <haotianwang92@gmail.com>
2013-02-11 21:28:17 -0800

    fixed parsing error in parsing unitTest output

--- app/controllers/testapi_controller.rb
+++ app/controllers/testapi_controller.rb
@@ -7,7 +7,12 @@ class TestapiController < ApplicationController
   def unitTests
     output = `rake db:migrate && rake db:test:prepare && ruby -Itest test/unit/user_test.rb`
     lines = output.split(/\n/)
-    lastLine = lines[lines.length-1]
+    lastLine = ""
+    lines.each do |line|
+        if line.include?("tests") && line.include?("assertions") && line.include?("failures") && line.include?("errors") && line.include?("skips")
+            lastLine = line
+        end
+    end
     chunks = lastLine.split(",")
     totalTests = Integer(chunks[0].gsub(" tests",""))
     nrFailed = Integer(chunks[2].gsub(" failures",""))
