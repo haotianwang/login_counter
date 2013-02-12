460965d21769c2dd3fa9d5c7b365e7f090648b34
haotian wang <haotianwang92@gmail.com>
2013-02-11 17:44:23 -0800

    added json for logins and adds, added renders and appropriate model method calls in the controllers, added api testing for testapi_controller, only stub

--- app/controllers/testapi_controller.rb
+++ app/controllers/testapi_controller.rb
@@ -1,7 +1,12 @@
 class TestapiController < ApplicationController
   def resetFixture
+    result = User.TESTAPI_resetFixture()
+    render :json => { 'errCode' => result }
   end
 
   def unitTests
+    cmd = "pwd"
+    output = `#{cmd}`
+    render :json => { 'output' => output }
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,22 @@
 class UsersController < ApplicationController
   def login
-    errCode = 1
     count = nil
     if request.method == "POST"
-
-    else
-        respond_to do |format|
-            format.html { redirect_to :action => "index" }
-        end
+        @errCode = User.login(params[:name], params[:password])
+        render :partial => "addOrLogin.json"
+    end
+    if request.method == "GET"
+        render :nothing => true
     end
   end
   
   def add
     if request.method == "POST"
-        respond_to do |format|
-            format.html { render action: "add"}
-        end
+        @errCode = User.add(params[:name], params[:password])
+        render :partial => "addOrLogin.json"
     end
     if request.method == "GET"
-        respond_to do |format|
-            format.html { render action: "index"}
-        end
+        render :nothing => true
     end
   end
 end
