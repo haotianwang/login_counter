6fcb4d4fdfc459d829b43281eb9e3f769f919d67
haotian wang <haotianwang92@gmail.com>
2013-02-17 18:27:37 -0800

    everything works

--- app/assets/javascripts/users/index.js
+++ app/assets/javascripts/users/index.js
@@ -1,10 +1,24 @@
+var SUCCESS=1;
+var ERR_BAD_CREDENTIALS=-1;
+var ERR_USER_EXISTS=-2;
+var ERR_BAD_USERNAME=-3;
+var ERR_BAD_PASSWORD=-4;
+
 $( document ).ready( function() {
     $("#loginButton").click(function(){
         login();
     });
     
     $("#signupButton").click(function(){
-        alert('clicked on signup!');
+        add();
+    });
+    
+    $("#logoutButton").click(function() {
+        $("#accountFields").css("display","none");
+        $("#loginFields").css("display","block");
+        $("#messageBox").html("Please enter your credentials below");
+        $("#usernameInput").val($("#usernameInput").css("placeholder"));
+        $("#passwordInput").val($("#passwordInput").css("placeholder"));
     });
 });
 
@@ -22,9 +36,37 @@ function login() {
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify({user: getUsername(), password: getPassword() }),
-        dataType: "json",
-        success: function(response) {
-            alert(response); // works as expected (returns all html)
+        dataType: "json"
+    }).done(function(data) {
+        if (data.errCode > 0) {
+            $("#accountFields").css("display","block");
+            $("#loginFields").css("display","none");
         }
+        $("#messageBox").html(getMessage(data));
     });
+}
+
+function add() {
+    $.ajax({
+        url: "users/add",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({user: getUsername(), password: getPassword() }),
+        dataType: "json"
+    }).done(function(data) {
+        if (data.errCode > 0) {
+            $("#accountFields").css("display","block");
+            $("#loginFields").css("display","none");
+        }
+        $("#messageBox").html(getMessage(data));
+    });
+}
+
+function getMessage(data) {
+    if (data.errCode == 1) return "Welcome " + getUsername() + ". You have logged in " + data.count + " times.";
+    if (data.errCode == -1) return "cannot find the user/password pair in the database";
+    if (data.errCode == -2) return "trying to add a user that already exists";
+    if (data.errCode == -3) return "invalid user name (user name should be non-empty and at most 128 ascii characters long)";
+    if (data.errCode == -4) return "invalid password (password should be at most 128 ascii characters)";
+    return "internal server error code " + data.errCode;
 }
